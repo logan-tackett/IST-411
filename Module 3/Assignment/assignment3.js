3b661bffe61fc58b413947949b810ecc8140bbce
logan-tackett <logan.tackett@doane.edu>
2021-03-03 00:46:39 -0600

    finalcommit

--- Module 3/Assignment/assignment3.js
+++ Module 3/Assignment/assignment3.js
@@ -12,15 +12,14 @@ function palindromecheck() {
         document.forms["myForm"]["newNumber"].value = "";
         return false;
     }
-    
     else {
         if (AlgorithimNumber==1){
             var tableRef = document.getElementById("algorithim1");
-            (tableRef.insertRow(tableRef.rows.length)).innerHTML = WordCheck;  
+            (tableRef.insertRow(tableRef.rows.length)).innerHTML = WordCheck + " : " + palindrome1(WordCheck);
         }
         else{
             var tableRef = document.getElementById("algorithim2");
-            (tableRef.insertRow(tableRef.rows.length)).innerHTML = WordCheck;
+            (tableRef.insertRow(tableRef.rows.length)).innerHTML = WordCheck + " : " + palindrome2(WordCheck);
         }
         document.forms["myForm"]["newWord"].value="";
         document.forms["myForm"]["newNumber"].value="";
@@ -37,4 +36,23 @@ function clearAlgorithim1(){
 function clearAlgorithim2(){
     var tableRef = document.getElementById("algorithim2");
     tableRef.innerHTML="";
+}
+
+function palindrome1(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+}
+
+function palindrome2(str) {
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
