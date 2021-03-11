514c8c25344e07075c1bced60be402c1f52eb2d6
logan-tackett <logan.tackett@doane.edu>
2021-03-11 00:48:17 -0600

    workingcommit4

---
+++ Module 4/Exercise/Exercise4.js
@@ -0,0 +1,21 @@
+async function getBaconipsum(){
+    var apiString = "https://baconipsum.com/api/";
+
+    var theNewParagraphs = document.getElementById("newParagraphs").value;
+    apiString = apiString + "?type=meat-and-filler&paras=" + theNewParagraphs;  
+    alert(apiString);
+
+    var response = await fetch(apiString);
+
+    document.getElementById("myRawData").innerHTML = "";
+    document.getElementById("myFormattedData").innerHTML = "";
+
+    var jsonData = await response.json();
+
+    document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
+
+    for(var para in jsonData){
+        document.getElementById("myFormattedData").innerHTML += "<p>" + jsonData[para] + "</p>";
+    }
+    return true;
+}
